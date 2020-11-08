8a518a552e03e82e02440cdfe9ca540d6165d16b
Robert Lewicki <lewicki.robert@onet.pl>
2020-11-08 11:56:11 +0100

    wątki

---
+++ src/test/java/pl/sda/finalapp/threads/OurRunnable.java
@@ -0,0 +1,9 @@
+package pl.sda.finalapp.threads;
+
+public class OurRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " OurRunnable");
+    }
+}
---
+++ src/test/java/pl/sda/finalapp/threads/ThreadsExample.java
@@ -0,0 +1,28 @@
+package pl.sda.finalapp.threads;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadsExample {
+
+    @Test
+
+    public void basicThreads(){
+        Runnable runnableClass = new OurRunnable();
+        Runnable runnableAnonymous = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " runnableAnonymous");
+            }
+        };
+
+        Runnable runnableLambda = ()->System.out.println(Thread.currentThread().getName() + " runnableLamda");
+
+        Thread thread1 = new Thread(runnableClass);
+        Thread thread2 = new Thread(runnableAnonymous);
+        Thread thread3 = new Thread(runnableLambda);
+       // thread1.run();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
