5bba746ed37617fdf47495e3e1da765e68189b42
Robert Lewicki <lewicki.robert@onet.pl>
2020-11-14 15:12:37 +0100

    regex rejestracja, poprawa info na stornie

--- src/main/java/pl/sda/finalapp/app/UserController.java
+++ src/main/java/pl/sda/finalapp/app/UserController.java
@@ -1,12 +1,20 @@
 package pl.sda.finalapp.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Map;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRegistartionValidationService validationService;
 
     @GetMapping("/registration")
     public String registrationForm(Model model){
@@ -16,7 +24,17 @@ public class UserController {
         return "registrationPage";
     }
     @PostMapping("/registration")
-    public String registrationEffect(UserRegistrationDTO userRegistrationDTO){
-        return "registrationPage";
+    public String registrationEffect(UserRegistrationDTO userRegistrationDTO, Model model){
+        Map<String, String> validationExceptionMap = validationService.validateUser(userRegistrationDTO);
+        if(!validationExceptionMap.isEmpty()){
+            model.addAllAttributes(validationExceptionMap);
+            model.addAttribute("userRegistrationData",userRegistrationDTO);
+            model.addAttribute("countryList",Countries.values());
+            return "registrationPage";
+
+        }
+        userService.registerUser(userRegistrationDTO);
+        model.addAttribute("email", userRegistrationDTO.geteMail());
+        return "welcomePage";
     }
 }
---
+++ src/main/java/pl/sda/finalapp/app/UserRegistartionValidationService.java
@@ -0,0 +1,19 @@
+package pl.sda.finalapp.app;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserRegistartionValidationService {
+
+    public Map<String, String> validateUser(UserRegistrationDTO dto) {
+        Map<String, String> exceptionsMap = new HashMap<>();
+        if (!dto.getFirstName().matches("^[A-Z][a-z]{2,}$")) {
+            exceptionsMap.put("firstNameValitadionResult",
+                    "Imię jest wymagane. Przynajmniej 3 znaki oraz pierwsza duża, reszta małe.");
+        }
+        return exceptionsMap;
+    }
+}
